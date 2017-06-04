d8d5428d49909248ebee9d120776a20f48dc8bf6
znergy <ryanjonesirl@gmail.com>
2017-06-04 16:25:18 -0700

    add the photo data model

---
+++ app/src/main/java/com/znergy/flickrbrowser/Photo.java
@@ -0,0 +1,47 @@
+package com.znergy.flickrbrowser;
+
+
+/*** Data Model ***/
+
+public class Photo {
+
+    private String title;
+    private String author;
+    private String authorId;
+    private String link;
+    private String tags;
+    private String image;
+
+    public Photo(String title, String author, String authorId, String link, String tags, String image) {
+        this.title = title;
+        this.author = author;
+        this.authorId = authorId;
+        this.link = link;
+        this.tags = tags;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
